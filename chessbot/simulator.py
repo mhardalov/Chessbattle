04a514d78bcaae3e8dce2109d6090c9eb3ffe56b
todor <t.markov@adcash.com>
2018-03-28 16:26:05 +0300

    Verbosity flag improved

--- chessbot/simulator.py
+++ chessbot/simulator.py
@@ -28,11 +28,11 @@ class ChessSimulator:
         self.players = [self.p1, self.p2]
         if shuffle: rnd.shuffle(self.players)
 
-    def print_board(self, svg, clear=False):
-        clear_output(wait=True)
+    def print_board(self, svg, clear=True):
+        if clear: clear_output(wait=True)
         display(svg)
 
-    def simulate(self, rounds=4, timeout=10, turn_sleep_ms=0, display_board_size=450, display_board=True):
+    def simulate(self, rounds=4, timeout=10, turn_sleep_ms=0, verbose_size=450, verbose=True):
         self.results = []
         for r in range(rounds):
             self.board.reset()
@@ -48,24 +48,24 @@ class ChessSimulator:
                     next_move = future.result(timeout=timeout)
                 self.board.push(next_move)
 
-                if display_board: self.print_board(SVG(chess.svg.board(board=self.board, size=display_board_size)))
+                if verbose: self.print_board(SVG(chess.svg.board(board=self.board, size=verbose_size)))
                 step += 1
                 if (turn_sleep_ms > 0):
                     time.sleep(turn_sleep_ms / 1000)
 
-            last_board = SVG(chess.svg.board(board=self.board, size=display_board_size))
+            last_board = SVG(chess.svg.board(board=self.board, size=verbose_size))
             winner_id = {'1-0': 0, '0-1': 1,'1/2-1/2': 2}[self.board.result()]
             winner = outcomes[winner_id]
             self.results.append((last_board, winner))
 
-        clear_output(wait=True)
+        if verbose: clear_output(wait=True)
         summary = {self.p1.get_name(): 0, self.p2.get_name(): 0}
         for (svg, winner) in self.results:
-            print('Game: {} vs {}. Winner: {}'
+            if verbose: print('Game: {} vs {}. Winner: {}'
                     .format(self.players[0].get_name(), self.players[1].get_name(), winner))
             if winner != self.DRAW: summary[winner] += 1.0
             else:
                 summary[self.p1.get_name()] += 0.5
                 summary[self.p2.get_name()] += 0.5
-            if display_board: display(svg)
+            if verbose: display(svg)
         return summary
