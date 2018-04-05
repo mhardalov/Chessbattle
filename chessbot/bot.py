e40e3b1fbc7c5aa3c1d04785d5f5c3a05ad46a78
Victor Velev <velev.victor@yahoo.com>
2018-04-05 19:30:52 +0300

    Add depth (number of moves) weighting

--- chessbot/bot.py
+++ chessbot/bot.py
@@ -63,7 +63,7 @@ class ChessBotVictor(ChessBot):
         self.depth = opt_dict['depth']
         self.is_white = True
 
-        self.pawns_table_white = [
+        self.pawns_eval_white = [
             0,  0,  0,  0,  0,  0,  0,  0,
             50, 50, 50, 50, 50, 50, 50, 50,
             10, 10, 20, 30, 30, 20, 10, 10,
@@ -74,9 +74,9 @@ class ChessBotVictor(ChessBot):
             0,  0,  0,  0,  0,  0,  0,  0
         ]
 
-        self.pawns_table_black = [i for i in reversed(self.pawns_table_white)]
+        self.pawns_eval_black = [i for i in reversed(self.pawns_eval_white)]
 
-        self.knights_table_white = [
+        self.knights_eval_white = [
             -50,-40,-30,-30,-30,-30,-40,-50,
             -40,-20,  0,  0,  0,  0,-20,-40,
             -30,  0, 10, 15, 15, 10,  0,-30,
@@ -87,9 +87,9 @@ class ChessBotVictor(ChessBot):
             -50,-40,-30,-30,-30,-30,-40,-50
         ]
 
-        self.knights_table_black = [i for i in reversed(self.knights_table_white)]
+        self.knights_eval_black = [i for i in reversed(self.knights_eval_white)]
 
-        self.bishops_table_white = [
+        self.bishops_eval_white = [
             -20,-10,-10,-10,-10,-10,-10,-20,
             -10,  0,  0,  0,  0,  0,  0,-10,
             -10,  0,  5, 10, 10,  5,  0,-10,
@@ -100,9 +100,9 @@ class ChessBotVictor(ChessBot):
             -20,-10,-10,-10,-10,-10,-10,-20
         ]
 
-        self.bishops_table_black = [i for i in reversed(self.bishops_table_white)]
+        self.bishops_eval_black = [i for i in reversed(self.bishops_eval_white)]
 
-        self.rooks_table_white = [
+        self.rooks_eval_white = [
              0,  0,  0,  0,  0,  0,  0,  0,
              5, 10, 10, 10, 10, 10, 10,  5,
             -5,  0,  0,  0,  0,  0,  0, -5,
@@ -113,9 +113,9 @@ class ChessBotVictor(ChessBot):
              0,  0,  0,  5,  5,  0,  0,  0
         ]
 
-        self.rooks_table_black = [i for i in reversed(self.rooks_table_white)]
+        self.rooks_eval_black = [i for i in reversed(self.rooks_eval_white)]
 
-        self.queen_table_white = [
+        self.queen_eval_white = [
             -20,-10,-10, -5, -5,-10,-10,-20,
             -10,  0,  0,  0,  0,  0,  0,-10,
             -10,  0,  5,  5,  5,  5,  0,-10,
@@ -126,9 +126,9 @@ class ChessBotVictor(ChessBot):
             -20,-10,-10, -5, -5,-10,-10,-20
         ]
         
-        self.queen_table_black = [i for i in reversed(self.queen_table_white)]
+        self.queen_eval_black = [i for i in reversed(self.queen_eval_white)]
 
-        self.king_table_white = [
+        self.king_eval_white = [
             -30,-40,-40,-50,-50,-40,-40,-30,
             -30,-40,-40,-50,-50,-40,-40,-30,
             -30,-40,-40,-50,-50,-40,-40,-30,
@@ -139,9 +139,9 @@ class ChessBotVictor(ChessBot):
              20, 30, 10,  0,  0, 10, 30, 20
         ]
 
-        self.king_table_black = [i for i in reversed(self.king_table_white)]
+        self.king_eval_black = [i for i in reversed(self.king_eval_white)]
 
-        self.king_end_game_table_white = [
+        self.king_end_game_eval_white = [
             -50,-40,-30,-20,-20,-30,-40,-50,
             -30,-20,-10,  0,  0,-10,-20,-30,
             -30,-10, 20, 30, 30, 20,-10,-30,
@@ -152,7 +152,7 @@ class ChessBotVictor(ChessBot):
             -50,-30,-30,-30,-30,-30,-30,-50
         ]
 
-        self.king_end_game_table_black = [i for i in reversed(self.king_end_game_table_white)]
+        self.king_end_game_eval_black = [i for i in reversed(self.king_end_game_eval_white)]
 
     def is_end_game(self, board):
         n_pawns = len(board.pieces(chess.PAWN, True)) + len(board.pieces(chess.PAWN, False))
@@ -162,12 +162,12 @@ class ChessBotVictor(ChessBot):
         n_queens = len(board.pieces(chess.QUEEN, True)) + len(board.pieces(chess.QUEEN, False))
         n_kings = len(board.pieces(chess.KING, True)) + len(board.pieces(chess.KING, False))
 
-        if n_pawns + n_knights + n_bishops + n_rooks + n_queens + n_kings < (8*4)/2:
+        if n_pawns + n_knights + n_bishops + n_rooks + n_queens + n_kings <= 22:
             return True
         else:
             return False
 
-    def calc_heuristic_score(self, board):
+    def calc_heuristic_score(self, board, depth):
         score = 0
 
         for i in range(8*8):
@@ -178,45 +178,45 @@ class ChessBotVictor(ChessBot):
             if piece.color == self.is_white:
                 if piece.piece_type == chess.PAWN:
                     if self.is_white:
-                        score += self.pawns_table_white[i]
+                        score += self.pawns_eval_white[i]
                     else:
-                        score += self.pawns_table_black[i]                    
+                        score += self.pawns_eval_black[i]                    
 
                 elif piece.piece_type == chess.KNIGHT:
                     if self.is_white:
-                        score += self.knights_table_white[i]
+                        score += self.knights_eval_white[i]
                     else:
-                        score += self.knights_table_black[i]                    
+                        score += self.knights_eval_black[i]                    
 
                 elif piece.piece_type == chess.BISHOP:
                     if self.is_white:
-                        score += self.bishops_table_white[i]
+                        score += self.bishops_eval_white[i]
                     else:
-                        score += self.bishops_table_black[i]                    
+                        score += self.bishops_eval_black[i]                    
 
                 elif piece.piece_type == chess.ROOK:
                     if self.is_white:
-                        score += self.rooks_table_white[i]
+                        score += self.rooks_eval_white[i]
                     else:
-                        score += self.rooks_table_black[i]                    
+                        score += self.rooks_eval_black[i]                    
 
                 elif piece.piece_type == chess.QUEEN:
                     if self.is_white:
-                        score += self.queen_table_white[i]
+                        score += self.queen_eval_white[i]
                     else:
-                        score += self.queen_table_black[i]                    
+                        score += self.queen_eval_black[i]                    
 
                 elif piece.piece_type == chess.KING and self.is_end_game(board):
                     if self.is_white:
-                        score += self.king_end_game_table_white[i]
+                        score += self.king_end_game_eval_white[i]
                     else:
-                        score += self.king_end_game_table_black[i]                    
+                        score += self.king_end_game_eval_black[i]                    
 
                 elif piece.piece_type == chess.KING:
                     if self.is_white:
-                        score += self.king_table_white[i]
+                        score += self.king_eval_white[i]
                     else:
-                        score += self.king_table_black[i]
+                        score += self.king_eval_black[i]
 
                 else:
                     pass
@@ -224,45 +224,45 @@ class ChessBotVictor(ChessBot):
             else:
                 if piece.piece_type == chess.PAWN:
                     if not self.is_white:
-                        score += self.pawns_table_white[i]
+                        score += self.pawns_eval_white[i]
                     else:
-                        score += self.pawns_table_black[i]                    
+                        score += self.pawns_eval_black[i]                    
 
                 elif piece.piece_type == chess.KNIGHT:
                     if not self.is_white:
-                        score += self.knights_table_white[i]
+                        score += self.knights_eval_white[i]
                     else:
-                        score += self.knights_table_black[i]                    
+                        score += self.knights_eval_black[i]                    
 
                 elif piece.piece_type == chess.BISHOP:
                     if not self.is_white:
-                        score += self.bishops_table_white[i]
+                        score += self.bishops_eval_white[i]
                     else:
-                        score += self.bishops_table_black[i]                    
+                        score += self.bishops_eval_black[i]                    
 
                 elif piece.piece_type == chess.ROOK:
                     if not self.is_white:
-                        score += self.rooks_table_white[i]
+                        score += self.rooks_eval_white[i]
                     else:
-                        score += self.rooks_table_black[i]                    
+                        score += self.rooks_eval_black[i]                    
 
                 elif piece.piece_type == chess.QUEEN:
                     if not self.is_white:
-                        score += self.queen_table_white[i]
+                        score += self.queen_eval_white[i]
                     else:
-                        score += self.queen_table_black[i]                    
+                        score += self.queen_eval_black[i]                    
 
                 elif piece.piece_type == chess.KING and self.is_end_game(board):
                     if not self.is_white:
-                        score += self.king_end_game_table_white[i]
+                        score += self.king_end_game_eval_white[i]
                     else:
-                        score += self.king_end_game_table_black[i]                    
+                        score += self.king_end_game_eval_black[i]                    
 
                 elif piece.piece_type == chess.KING:
                     if not self.is_white:
-                        score += self.king_table_white[i]
+                        score += self.king_eval_white[i]
                     else:
-                        score += self.king_table_black[i]
+                        score += self.king_eval_black[i]
 
                 else:
                     pass
@@ -303,11 +303,13 @@ class ChessBotVictor(ChessBot):
         if len(board.pieces(chess.PAWN, self.is_white)) < len(board.pieces(chess.PAWN, (not self.is_white))): # Looses Pawn
             score += -100
 
+        # Makes AI smarter with weighting depth (number of moves)
+        score += (depth-1) * 100
         return score
 
     def minimax(self, board, depth, alpha, beta):
         if depth == 1 or board.is_checkmate():
-            return self.calc_heuristic_score(board)
+            return self.calc_heuristic_score(board, depth)
 
         elif board.turn == self.is_white:
             v = -10**6
